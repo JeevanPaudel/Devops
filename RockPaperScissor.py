05607b3cfb9bed19c41f270df82669fa4380d56c
Jeevan Paudel <87184445+JeevanPaudel@users.noreply.github.com>
2021-10-25 22:37:17 +1030

    Add files via upload

---
+++ RockPaperScissor.py
@@ -0,0 +1,21 @@
+import random
+
+def game():
+    user = input("Choose 's' for SCISSOR, 'p' for PAPER,'r' for ROCK :")
+    comp = random.choice(('s', 'p', 'r'))
+
+    if user == comp:
+        print("It's a tie !!")
+
+    elif win(user, comp):                          #Game theory s>p,r>s,p>r
+        print("You Won!!")
+    else:
+        print("Computer won")
+
+
+def win(user, comp):
+    #wins when s>p,r>s,p>r
+    if (user =='s' and comp == 'p') or (user =='r' and comp == 's') or (user =='p' and comp == 'r'):
+        return True                      #True is returned if player wins
+
+game()
